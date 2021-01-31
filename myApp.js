f6fa3231ba219cbd76269b4975fe9f9341961a90
Jose Lopez <joseglopez1289@gmail.com>
2021-01-31 08:17:50 -0800

    Initialize repository

--- myApp.js
+++ myApp.js
@@ -1,6 +1,29 @@
 var express = require('express');
 var app = express();
 
+// 1. Meet the Node console
+
+// 2.Start a Working Express Server
+
+// 3. Serve an HTML File
+
+// 4. Serve Static Assets
+
+// 5. Serve JSON on a Specifc Route
+
+// 6. Use the .env File
+
+// 7. Implement a Root-Level Request Logger Middleware
+
+// 8. Chain Middleware to Create a Time Server
+
+// 9. Get Route Parameter Input from the Client
+
+// 10. Get Query Parameter Input from the Client
+
+// 11. Use body-parser to Parse POST Requests
+
+// 12. Get Data from POST Requests
 
 
 
@@ -36,4 +59,4 @@ var app = express();
 
 
 
- module.exports = app;
+module.exports = app;
